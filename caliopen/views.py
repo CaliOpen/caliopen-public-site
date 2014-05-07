a1cd27644f0d20348906e723a1aec2915662e119
Vincent Mineaud <vincent@gandi.net>
2014-05-07 15:05:26 +0200

    Add page contact

--- caliopen/views.py
+++ caliopen/views.py
@@ -1,8 +1,20 @@
 from pyramid.view import view_config
+from pyramid.i18n import TranslationString
 
 
 @view_config(route_name='home', renderer='templates/layout/home.pt')
 def my_view(request):
+    return {
+        'project': 'caliopen',
+		'page_description': 'CaliOpen, be safe.',
+		'page_title': 'CaliOpen, be safe.',
+		'page_author': 'Gandi'
+    }
+
+@view_config(route_name='contact', renderer='templates/layout/contact.pt')
+def contact(request):
+    ts = TranslationString('Add')
+
     return {
         'project': 'caliopen',
 		'page_description': 'CaliOpen, be safe.',
