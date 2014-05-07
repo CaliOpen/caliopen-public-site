7d8f485cc3e0e510c9878b358e37c6fdd9daa64b
Alexis Mineaud <alexis@gandi.net>
2014-05-07 12:20:21 +0200

    Use controller variable.

--- caliopen/views.py
+++ caliopen/views.py
@@ -3,7 +3,9 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/layout/home.pt')
 def my_view(request):
-    return {'project': 'caliopen',
-			'page_description': 'Description de Caliopen',
-			'page_title': 'Site vitrine de Caliopen',
-			'page_author': 'Gandi'}
+    return {
+        'project': 'caliopen',
+		'page_description': 'CaliOpen, be safe.',
+		'page_title': 'CaliOpen, be safe.',
+		'page_author': 'Gandi'
+    }
