7bd353a9a6a7155d27e011354e84c3f92e775913
Vincent Mineaud <vincent@gandi.net>
2014-05-13 12:19:09 +0200

    Fix: configuration to use translations with jinja2.

--- caliopen/__init__.py
+++ caliopen/__init__.py
@@ -1,12 +1,15 @@
 from pyramid.config import Configurator
 
-
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    settings = dict(settings)
+    settings.setdefault('jinja2.i18n.domain', 'caliopen')
+
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_translation_dirs('locale/')
     config.add_route('home', '/')
     config.add_route('contact', '/contact')
     config.add_route('features', '/features')
