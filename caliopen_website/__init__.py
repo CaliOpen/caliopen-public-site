0c5528eb92b33f6d97cc79dc24999e0a4bc11c68
Guillaume Gauvrit <guillaume@gauvr.it>
2014-05-13 22:13:20 +0200

    Use assets instead of static

--- caliopen_website/__init__.py
+++ caliopen_website/__init__.py
@@ -9,7 +9,7 @@ def main(global_config, **settings):
 
     views.use_less = settings['caliopen_ws.css'] == 'less'
 
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('assets', 'assets', cache_max_age=3600)
     config.add_translation_dirs('locale/')
     config.add_route('home', '/')
     config.add_route('contact', '/contact')
