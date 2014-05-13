284e79369ef7a82ad192fa591195394382b3e585
Vincent Mineaud <vincent@gandi.net>
2014-05-13 13:53:54 +0200

    Move "jinja2" confs in ini files.

--- caliopen/__init__.py
+++ caliopen/__init__.py
@@ -3,11 +3,7 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    settings = dict(settings)
-    settings.setdefault('jinja2.i18n.domain', 'caliopen')
-
     config = Configurator(settings=settings)
-    config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_translation_dirs('locale/')
     config.add_route('home', '/')
