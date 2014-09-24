01aeb40c9fa5490d416d83bfd4ff64aaa74a0bba
Aymeric Barantal <mric@gandi.net>
2014-09-24 12:41:46 +0200

    move locale_negociator into .lang package

--- caliopen_website/__init__.py
+++ caliopen_website/__init__.py
@@ -1,25 +1,6 @@
 from pyramid.config import Configurator
 from . import views
-
-
-LANGUAGES = {
-        'en-CA': 'en',
-        'en-GB': 'en',
-        'en-US': 'en',
-        'en': 'en',
-        'fr-FR': 'fr',
-        'fr-BE': 'fr',
-        'fr': 'fr',
-        }
-
-
-def locale_negotiator(request):
-    """Locale negotiator base on the `Accept-Language` header"""
-    locale = 'en'
-    if request.accept_language:
-        locale = request.accept_language.best_match(LANGUAGES)
-        locale = LANGUAGES.get(locale, 'en')
-    return locale
+from .lang import locale_negotiator
 
 
 def main(global_config, **settings):
