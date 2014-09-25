804d820b389e5b731036641da3266b24356154e1
Aymeric Barantal <mric@gandi.net>
2014-09-25 15:18:13 +0200

    use lang= in query string to test easily others languages

--- caliopen_website/lang.py
+++ caliopen_website/lang.py
@@ -13,6 +13,8 @@ LANGUAGES = {
 def locale_negotiator(request):
     """Locale negotiator base on the `Accept-Language` header"""
     locale = 'en'
+    if 'lang' in request.params:
+        return request.params['lang']
     if request.accept_language:
         locale = request.accept_language.best_match(LANGUAGES)
         locale = LANGUAGES.get(locale, 'en')
